3374f39a8ba0efd2048fa8c9dc4c8d76b7cda82e
yurenpang <ypang@macalester.edu>
2020-03-17 05:35:28 -0500

    updated.

--- static/js/app.js
+++ static/js/app.js
@@ -4,16 +4,19 @@ $(document).ready(function() {
         url: "/query",
         async: true
     }).done(function(data) {
+
         if(data.error) {
             alert("Cannot render data");
         } else {
             $("tbody").empty()
 
+            alert("Hii")
+
             for(let i = 0; i < data.length; i++) {
                 $("tbody").append("<tr>" +
                 "<td>" + str(i) + "</td>" +
-                "<td>" + data[i][ip] + "</td>" +
-                "<td>" + data[i][text] + "</td>" +
+                "<td>" + data[i]['ip'] + "</td>" +
+                "<td>" + data[i]['text'] + "</td>" +
                 "</tr>")
             }
         }
